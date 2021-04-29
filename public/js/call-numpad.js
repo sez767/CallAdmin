0b9142dc7afdfc301f5acddf900a34a9845d3b1a
sez767 <sez767@gmail.com>
2021-04-29 11:44:19 +0300

    fx

--- public/js/call-numpad.js
+++ public/js/call-numpad.js
@@ -202,7 +202,7 @@ if(configuration.uri && configuration.password){
             completeSession();
             if(accountRole == 'user'){
                 $('#callInfoText').val(`Все операторы заняты, пожалуйста ожидайте...(${refreshes})`);
-                if(refreshes>1){
+                if(refreshes>0){
                     setTimeout(function() {
                         sessionStorage.setItem('refreshes', --refreshes);
                         window.location.reload(false);
@@ -311,7 +311,7 @@ function callC() {
         updateUI();
     }else{
         $('#callInfoText').val(`Все операторы заняты, пожалуйста ожидайте...(${refreshes})`);
-        if(refreshes>1){
+        if(refreshes>0){
             setTimeout(function() {
                 sessionStorage.setItem('refreshes', --refreshes);
                 window.location.reload(false);
